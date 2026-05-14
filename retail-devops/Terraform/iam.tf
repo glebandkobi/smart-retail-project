4895d6e58d9a11c571bca828288d59839a4f47b9
Koby Gomel <kgomel1@gmail.com>
2026-05-14 17:06:01 -0400

    Add Flask inventory API

---
+++ retail-devops/Terraform/iam.tf
@@ -0,0 +1,9 @@
+# Permission for the GitHub Actions Robot
+resource "aws_iam_user" "github_actions" {
+  name = "github-actions-deployer"
+}
+
+resource "aws_iam_user_policy_attachment" "admin" {
+  user       = aws_iam_user.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
