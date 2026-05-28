e9a52c41b6ae8918e9545e9e0ccbc626d173c257
skalgleb-devops <glebskal@outlook.com>
2026-05-28 19:09:56 +0300

    Fix Terraform module versions, upgrade engine, and add AWS AZs

--- retail-devops/Terraform/Infrastructure.tf
+++ retail-devops/Terraform/Infrastructure.tf
@@ -1,24 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 # AWS VPC and EKS Cluster Provisioning
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "retail-vpc"
-  cidr   = "10.0.0.0/16"
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name   = "retail-vpc"
+  cidr   = "10.0.0.0/16"
+  
+  # THIS IS THE MISSING LINE
+  azs            = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-  cluster_name    = "retail-cluster"
-  cluster_version = "1.27"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.public_subnets
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
 
-  eks_managed_node_groups = {
-    warehouse_nodes = {
-      min_size     = 2
-      max_size     = 4
-      instance_types = ["t3.medium"]
-    }
-  }
+  cluster_name    = "retail-cluster-2"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    warehouse_nodes = {
+      min_size       = 2
+      max_size       = 4
+      instance_types = ["t3.medium"]
+    }
+  }
 }
