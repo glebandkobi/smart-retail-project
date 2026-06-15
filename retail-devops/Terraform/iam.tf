3d56ac075207c34d897484d29832897294d92f74
glebandkoby <kobyandgleb@outlook.com>
2026-06-15 17:57:42 +0300

    Update iam.tf

--- retail-devops/Terraform/iam.tf
+++ retail-devops/Terraform/iam.tf
@@ -1,6 +1,6 @@
 # Permission for the GitHub Actions Robot
 resource "aws_iam_user" "github_actions" {
-  name = "github-actions-deployer"
+  name = "github-actions-deployer-v8"
 }
 
 resource "aws_iam_user_policy_attachment" "admin" {
